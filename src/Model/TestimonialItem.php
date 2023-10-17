d32fa020ee5dbe85d7bf9fac70d130f954bf65d8
Tim Burt <tim@biffbangpow.com>
2023-10-17 14:47:15 +0100

    Base code

---
+++ src/Model/TestimonialItem.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BiffBangPow\Element\Model;
+
+use BiffBangPow\Element\TestimonialsElement;
+use BiffBangPow\Extension\SortableExtension;
+use SilverStripe\Assets\Image;
+use SilverStripe\ORM\DataObject;
+
+/**
+ * Class \BiffBangPow\Element\Model\TestimonialItem
+ *
+ * @property int $Sort
+ * @property string $Quote
+ * @property string $Attribution
+ * @property int $ElementID
+ * @property int $ImageID
+ * @method \BiffBangPow\Element\TestimonialsElement Element()
+ * @method \SilverStripe\Assets\Image Image()
+ * @mixin \BiffBangPow\Extension\SortableExtension
+ */
+class TestimonialItem extends DataObject
+{
+    private static $table_name = 'BBP_TestimonialsElementItem';
+    private static $has_one = [
+        'Element' => TestimonialsElement::class,
+        'Image' => Image::class
+    ];
+    private static $owns = [
+        'Image'
+    ];
+    private static $extensions = [
+        SortableExtension::class
+    ];
+    private static $db = [
+        'Quote' => 'Text',
+        'Attribution' => 'Varchar'
+    ];
+    private static $summary_fields = [
+        'Image.CMSThumbnail' => 'Image',
+        'Attribution' => 'Attribution',
+        'Quote.LimitWordCount' => 'Quote'
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName(['ElementID']);
+        return $fields;
+    }
+}
---
+++ src/TestimonialsElement.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BiffBangPow\Element;
+
+use BiffBangPow\Element\Model\TestimonialItem;
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+
+/**
+ * Class \BiffBangPow\Element\TestimonialsElement
+ *
+ * @method \SilverStripe\ORM\DataList|\BiffBangPow\Element\Model\TestimonialItem[] Testimonials()
+ */
+class TestimonialsElement extends BaseElement
+{
+    private static $table_name = 'BBP_TestimonialsElement';
+    private static $inline_editable = false;
+
+    private static $has_many = [
+        'Testimonials' => TestimonialItem::class
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName(['Testimonials']);
+        $fields->addFieldsToTab('Root.Main', [
+           GridField::create(
+               'Testimonials',
+               _t(__CLASS__.'.Testimonials', 'Testimonials'),
+               $this->Testimonials(),
+               GridFieldConfig_RecordEditor::create()->addComponent(GridFieldOrderableRows::create())
+           )
+        ]);
+        return $fields;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return 'Testimonials';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSimpleClassName()
+    {
+        return 'bbp-testimonials-element';
+    }
+}
